b787222d5d7638f6eaa13ee6179e2e281a28d60e
Ryan Waits <ryan.waits@gmail.com>
2023-03-24 13:06:07 -0500

    add disclaimer

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,34 @@ export default function RootLayout({
           raleway.variable
         )}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <div className='flex items-center gap-x-6 bg-gray-900 py-2.5 px-6 sm:px-3.5 sm:before:flex-1'>
+            <p className='text-sm leading-6 text-white'>
+              <a href='#'>
+                <svg
+                  viewBox='0 0 2 2'
+                  className='mx-2 inline h-0.5 w-0.5 fill-current'
+                  aria-hidden='true'
+                >
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <strong className='font-semibold'>
+                  Currently in Beta and may not always return information
+                  reflected in source material
+                </strong>
+                <svg
+                  viewBox='0 0 2 2'
+                  className='mx-2 inline h-0.5 w-0.5 fill-current'
+                  aria-hidden='true'
+                >
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+              </a>
+            </p>
+            <div className='flex flex-1 justify-end'></div>
+          </div>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
