e394caea44fd4473bd5f4b2646315df9188f9d0b
Ryan Waits <ryan.waits@gmail.com>
2023-03-24 11:45:03 -0500

    disable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
+  darkMode: false, // or 'media' or 'class'
   content: [
     'app/**/*.{ts,tsx}',
     'components/**/*.{ts,tsx}',
